6333e1a8ee8b1f42ac306920353f62d5a01375da
alitayin <alitaweb3@gmail.com>
2025-02-11 20:16:26 +0800

    better log option

--- src/lib/Logger.js
+++ src/lib/Logger.js
@@ -1,17 +1,33 @@
 class Logger {
     constructor(enableLogging = false) {
         this.enableLogging = enableLogging;
+        this.timers = new Map();
     }
 
     log(...args) {
         if (this.enableLogging) {
-            console.log(...args);
+            console.log(new Date().toISOString(), ...args);
         }
     }
 
     error(...args) {
         if (this.enableLogging) {
-            console.error(...args);
+            console.error(new Date().toISOString(), ...args);
+        }
+    }
+
+    startTimer(label) {
+        this.timers.set(label, Date.now());
+        this.log(`${label} timer started`);
+    }
+
+    endTimer(label) {
+        if (this.timers.has(label)) {
+            const elapsed = Date.now() - this.timers.get(label);
+            this.log(`${label} timer ended: ${elapsed} ms`);
+            this.timers.delete(label);
+        } else {
+            this.log(`${label} timer not found`);
         }
     }
 }
